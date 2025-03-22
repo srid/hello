5546036179eb5edbf7c80491932ce058e759db14
Sridhar Ratnakumar <srid@srid.ca>
2025-03-22 13:30:41 -0400

    Add simple web app, returning Hello World

--- README.md
+++ README.md
@@ -1,1 +1,9 @@
 # hello
+
+Just a Hello World web app in Haskell for various demo purposes.
+
+## Running
+
+```sh
+nix run . -- --host 0.0.0.0 --port 8080
+```
--- src/Main.hs
+++ src/Main.hs
@@ -1,20 +1,55 @@
 module Main where
 
-import Main.Utf8 qualified as Utf8
+import Network.HTTP.Types (status200)
+import Network.HTTP.Types.Header (hContentType)
+import Network.Wai (Application, responseLBS)
+import Network.Wai.Handler.Warp (Port, run)
+import Options.Applicative
 
-data Example = Example
-  { name :: Text
-  , age :: Int
-  }
-  deriving stock (Show, Eq)
-
-{- |
- Main entry point.
-
- `just run` will invoke this function.
--}
 main :: IO ()
 main = do
-  -- For withUtf8, see https://serokell.io/blog/haskell-with-utf8
-  Utf8.withUtf8 $ do
-    putTextLn "Hello 🌎 (from hello)"
+  Options {host, port} <- execParser opts
+  putStrLn $ "Starting server on http://" <> host <> ":" <> show port <> "..."
+  run port app
+
+app :: Application
+app _req respond =
+  respond $
+    responseLBS
+      status200
+      [(hContentType, "text/plain")]
+      "Hello world"
+
+-- Command-line options data type
+data Options = Options
+  { host :: String
+  , port :: Port
+  }
+
+-- Parser for command-line options
+optionsParser :: Parser Options
+optionsParser =
+  Options
+    <$> strOption
+      ( long "host"
+          <> metavar "HOST"
+          <> help "Host address to bind to (e.g., 0.0.0.0)"
+          <> value "127.0.0.1"
+      )
+    <*> option
+      auto
+      ( long "port"
+          <> metavar "PORT"
+          <> help "Port number to listen on (e.g., 3000)"
+          <> value 3000
+      )
+
+-- Parser info with additional metadata
+opts :: ParserInfo Options
+opts =
+  info
+    (optionsParser <**> helper)
+    ( fullDesc
+        <> progDesc "Run a simple web server serving 'Hello world'"
+        <> header "Hello World Web App"
+    )
